1e179a86e2937ed0a436cd43a4ec1df1362d2202
{979896359@qq.com} <qydou@zufe.edu.cn>
2023-02-03 11:45:15 +0800

    demo

---
+++ args/args.py
@@ -0,0 +1,8 @@
+# ethereum node ws connection by infra
+infura_ws_url = "wss://mainnet.infura.io/ws/v3/4c8b73b0442b4953b4ee091ab44b3c90"
+# ethereum node http connection by infra
+infura_http_url = "https://mainnet.infura.io/v3/4c8b73b0442b4953b4ee091ab44b3c90"
+# etherscan API KEY
+api_key = "WAPS13EKFGJ7WYTSPHAZVFXR5GTNHPH4BY"
+# base url to etherscan
+base_url = "https://api.etherscan.io/api"
